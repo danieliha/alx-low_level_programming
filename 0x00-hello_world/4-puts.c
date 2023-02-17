1933c56af012d5abcf7c2e0e94460c609d7e534a
danieliha <daniel.nyundo@gmail.com>
2023-02-17 04:14:18 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts funtion
- * Return: 0 (Success)
+ * main - entry point
+ * Return: always 0 (Success)
  */
 int main(void)
 {
