8a8872cb083eab01e75fdad29566e1199c8b912f
danieliha <daniel.nyundo@gmail.com>
2023-02-16 19:14:06 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <studio>
+#include <studio.h>
 /**
  * main - A program that print a line using printf function
  * Return: 0 (success)
@@ -6,5 +6,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	return (0);
 }
