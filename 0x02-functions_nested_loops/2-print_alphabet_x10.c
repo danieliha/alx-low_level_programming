a156c38321cac6b24bd8bb711f473ce5f0d3a030
danieliha <daniel.nyundo@gmail.com>
2023-02-21 12:56:07 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,18 @@
 /**
  * print_alphabet_x10 - prints alpha 10 times
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-	int 10;
-	char letters;
+	int i = 0;
+	char c;
 
-	for (10 = 0; 10 <= 9; 10++)
+	while (i <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}	_putchar('\n');
+		i++;
 	}
 }
