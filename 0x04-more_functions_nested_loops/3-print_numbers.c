7ba1f1b42f0fe7e01f2f742f95c762a54f835286
danieliha <daniel.nyundo@gmail.com>
2023-02-24 14:20:56 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * print_numbers - print the numbers since 0 up to 9
@@ -10,8 +11,9 @@ void print_numbers(void)
 	int x;
 
 	for (x = 0; x <= 9; x++)
+
 	{
-		_putchar(x + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
