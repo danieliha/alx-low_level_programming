9efaf973cc4efe0421af7e73a95177da4c010ffe
danieliha <daniel.nyundo@gmail.com>
2023-03-08 23:26:19 -0800

    Task 0

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -gets the length of the string
+ * @s: string whose length is to be calculated
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (*s ? (1 + _strlen_recursion(++s)) : 0);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include  <stdlib.h>
+#include <stdio.h>
+
+/**
+ * number - help find the square root of natural squareroot numbers.
+ * 2n: natural number starting from 1
+ * @root: squareroot number to be found.
+ * 
+ * Return the root of the answer.
+ * natural square root return -1
+ * if root less than 0 return -1
+ */
+int find_sqrt(int num,int root)
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return(number(1, n));
+}
